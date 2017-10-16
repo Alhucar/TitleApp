6c38df122f655339a489b6c32300dc93c3273d95
Alvaro <ahc_frisdal@hotmail.com>
2017-10-16 16:29:23 +0200

    versio feta a clase

--- app/src/main/java/edu/upc/eseiaat/pma/titleapp/TittleActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/titleapp/TittleActivity.java
@@ -1,13 +1,44 @@
 package edu.upc.eseiaat.pma.titleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TittleActivity extends AppCompatActivity {
+
+    private String title = "Aquest es el titol inicial...";
+    private TextView title_text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tittle);
+        title_text = (TextView) findViewById(R.id.title);
+        title_text.setText(title);
+    }
+    public void editTitle(View view) {
+        //(I)
+        //1. crear un 'intent'
+        Intent intent = new Intent(this, EditTitleActivity.class);
+        //2. Afegir parametres (dades extra) a la crida a l'activitat
+        intent.putExtra("title",title);
+        //3. passar l'intent a Android perquè obri l'activitat
+        startActivity(intent);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case 0:
+                //(IV)
+                if (resultCode == AppCompatActivity.RESULT_OK){
+                    title = data.getStringExtra("title");
+                    title_text.setText(title);
+                }
+        }
     }
 }
